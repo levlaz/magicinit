1478eb9ca6ef827a8714c140487cb4d9524a342e
Vasek - Tom C <tom.chauveau@epitech.eu>
2024-10-01 11:36:58 -0700

    feat: link go template to genration
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- main.go
+++ main.go
@@ -15,9 +15,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"dagger/magicinit/internal/dagger"
 	"fmt"
+	"text/template"
 )
 
 type Magicinit struct{}
@@ -34,7 +36,17 @@ func (m *Magicinit) Init(
 	//+optional
 	provider string,
 ) (*dagger.Directory, error) {
-	return source, nil
+	inspection, err := m.Inspect(ctx, source)
+	if err != nil {
+		return nil, fmt.Errorf("Magicinit.Init: failed to inspect the source directory: %w", err)
+	}
+
+	switch inspection.Language {
+	case "go":
+		return m.initGo(ctx, inspection)
+	default:
+		return nil, fmt.Errorf("Magicinit.Init: unsupported language %s", inspection.Language)
+	}
 }
 
 func (m *Magicinit) Inspect(
@@ -54,9 +66,36 @@ func (m *Magicinit) Inspect(
 		if !isLanguage {
 			continue
 		}
-		
+
 		return inspector.Inspect(ctx, source)
 	}
 
 	return nil, fmt.Errorf("Magicinit.Inspect: failed to inspect the source directory: no language found")
+}
+
+func (m *Magicinit) initGo(ctx context.Context, inspection *SourceInspect) (*dagger.Directory, error) {
+	goTemplateDir := dag.CurrentModule().Source().Directory("templates/go")
+	goTemplate, err := goTemplateDir.File("main.go.tmpl").Contents(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("Magicinit.initGo: failed to read go template: %w", err)
+	}
+
+	var goTemplateData = struct {
+		GoVersion string
+	}{
+		GoVersion: inspection.Version,
+	}
+
+	tmpl, err := template.New("go.tmpl").Parse(goTemplate)
+	if err != nil {
+		return nil, fmt.Errorf("Magicinit.initGo: failed to parse go template: %w", err)
+	}
+
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, goTemplateData)
+	if err != nil {
+		return nil, fmt.Errorf("Magicinit.initGo: failed to execute go template: %w", err)
+	}
+
+	return goTemplateDir.WithNewFile("main.go", buf.String()).WithoutFile("main.go.tmpl"), nil
 }
